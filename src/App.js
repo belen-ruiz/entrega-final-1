dcc806657e5efea0ddb40415702d06c717badd14
belen ruiz <beelen.ruiz91@gmail.com>
2023-02-14 20:24:51 -0300

    entrega final

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {  } from "react-router-dom";
+import { NavBar } from "./components";
+
+
+
+function App() {
+  return (
+    
+    <header className="header">
+
+      <NavBar />
+
+    </header>
+      
+  );
+}
+
+export default App;
